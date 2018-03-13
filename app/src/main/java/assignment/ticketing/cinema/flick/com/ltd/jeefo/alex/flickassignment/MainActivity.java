5829e5ff2617ed1037b1f961df91db2833069013
Alexandru Dochioiu <2061919d@student.gla.ac.uk>
2018-03-13 14:23:57 +0000

    new project

---
+++ app/src/main/java/assignment/ticketing/cinema/flick/com/ltd/jeefo/alex/flickassignment/MainActivity.java
@@ -0,0 +1,13 @@
+package assignment.ticketing.cinema.flick.com.ltd.jeefo.alex.flickassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
